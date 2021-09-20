f5454c7235732c9137f9645efd966ed7c26d76be
Xavi <57891264+Xavi-PL@users.noreply.github.com>
2021-09-20 21:11:37 +0200

    Added BinancyDateDialog

---
+++ lib/utils/dialogs/date_dialog.dart
@@ -0,0 +1,38 @@
+import 'package:binancy/utils/ui/styles.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BinancyDatePicker {
+  final BuildContext context;
+  final DateTime initialDate;
+  final DateTime? firstDate;
+  final DateTime? lastDate;
+
+  BinancyDatePicker(
+      {required this.context,
+      required this.initialDate,
+      this.firstDate,
+      this.lastDate});
+
+  Future<DateTime?> showCustomDialog() async {
+    return showDatePicker(
+            locale: Localizations.localeOf(context),
+            context: context,
+            builder: (context, child) => Theme(
+                data: ThemeData.light().copyWith(
+                  primaryColor: accentColor,
+                  colorScheme: ColorScheme.light(primary: secondaryColor),
+                  buttonTheme:
+                      const ButtonThemeData(textTheme: ButtonTextTheme.primary),
+                ),
+                child: child ?? Container()),
+            initialDate: initialDate,
+            firstDate: firstDate ?? DateTime(1970),
+            lastDate: lastDate ?? DateTime(2100))
+        .then((value) async {
+      if (value != null) {
+        return value;
+      }
+    });
+  }
+}
