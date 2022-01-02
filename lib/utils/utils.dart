270499a8e8de9a45fe80b2bf1e1e01fc7d2ecf8e
Xavi <57891264+Xavi-PL@users.noreply.github.com>
2022-01-02 22:42:45 +0100

    Fixed bugs with MovementsBalanceView related to new year

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -265,8 +265,12 @@ class Utils {
 
   static Month getMonthNameOfPayDay(DateTime thisMonthPayday) {
     bool isAfterHalfMonth = thisMonthPayday.day >= 16;
-    return Month.values[
-        isAfterHalfMonth ? thisMonthPayday.month + 1 : thisMonthPayday.month];
+    if (thisMonthPayday.month == Month.values.length - 1) {
+      return Month.values[isAfterHalfMonth ? 1 : 12];
+    } else {
+      return Month.values[
+          isAfterHalfMonth ? thisMonthPayday.month + 1 : thisMonthPayday.month];
+    }
   }
 
   static int getPayDayOfMonth(DateTime date) {
